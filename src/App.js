b30429817208ebd138c181f4d2595f44c62e8f5c
Somanath Goudar <somugowda8050@gmail.com>
2020-12-03 14:35:45 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import alanBtn from '@alan-ai/alan-sdk-web'
+import wordsToNumbers from 'words-to-numbers'
+import NewsCards from './components/NewsCards/NewsCards'
 
-function App() {
+import useStyles from './styles.js'
+
+const alanKey = '4dd4c7d0361ac7b4e8f1b201013ac1792e956eca572e1d8b807a3e2338fdd0dc/stage'
+
+const App = () => {
+  const [newsArticles, setNewsArticles] = useState([])
+  const [activeArticle, setActiveArticle] = useState(-1)
+  const classes = useStyles()
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles)
+          setActiveArticle(-1)
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1)
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number
+          const article = articles[parsedNumber - 1]
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again.')
+          } else if (article) {
+            window.open(article.url, '_blank')
+            alanBtn().playText('Opening')
+          }
+        }
+      },
+    })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={classes.logoContainer}>
+        <img src='https://alan.app/voice/images/previews/preview.jpg' className={classes.alanLogo} alt='alan logo' />
+      </div>
+      <div>
+        <center>
+          <h2>Created by Somanath Goudar with Alan AI</h2>
+        </center>
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
